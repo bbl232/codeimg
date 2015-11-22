75fde2c0fc53ff0bdfadf60c5708a53dc940277d
Bill Vandenberk <bill@vandenberk.me>
2015-11-21 19:44:42 -0500

    removing Connect.php

--- Connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  $db = new mysqli("127.0.0.1", "codeimg", "serpentineRaspberries47", "codeimg", 3306);
-  if ($db->connect_errno) {
-      echo json_encode(array("error"=>"Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error));
-  }
-?>
