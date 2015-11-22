6a0a79485e39b5247f4875968684a7c2038c8854
Bill Vandenberk <bill@vandenberk.me>
2015-11-22 00:06:54 -0500

    ignoring config.php (filled in by rake)

--- www/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("LDAP_SERVER","fluffy.socs.uoguelph.ca");
-define("LDAP_PORT",389);
-define("LDAP_BASEDN","ou=People,dc=socs,dc=uoguelph,dc=ca");
-
-define("BASE_HOSTNAME","codeimg.me");
-define("FQDN","https:".BASE_HOSTNAME);
-?>
-
